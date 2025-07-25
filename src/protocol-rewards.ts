dd1a68878be0b8a7b1cd4d0db7a00814f31ff9d2
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2025-07-25 10:54:17 +0100

    Include zora reward origin

--- src/protocol-rewards.ts
+++ src/protocol-rewards.ts
@@ -1,107 +1,80 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, log } from "@graphprotocol/graph-ts";
 import {
-  Deposit as DepositEvent,
   RewardsDeposit as RewardsDepositEvent,
-  Withdraw as WithdrawEvent
 } from "../generated/ProtocolRewards/ProtocolRewards"
 import {
   User,
 } from "../generated/schema"
 
-export function handleDeposit(event: DepositEvent): void {
-  let fromUser = User.load(event.params.from.toHexString());
-  if (!fromUser) {
-    fromUser = createUser(event.params.from.toHexString(), event.block.timestamp, event.address.toHexString());
-  }
-  let toUser = User.load(event.params.to.toHexString());
-  if (!toUser) {
-    toUser = createUser(event.params.to.toHexString(), event.block.timestamp, event.address.toHexString());
-  }
-
-  toUser.rewardsTotal = toUser.rewardsTotal.plus(event.params.amount);
-  toUser.updatedAt = event.block.timestamp;
-  toUser.save();
-}
+const PODS_ADDRESS = "0x1c0e93f01b65fbc938b67a96f1c26bc27fd356a9".toLowerCase();
 
 export function handleRewardsDeposit(event: RewardsDepositEvent): void {
-  let creatorUser = User.load(event.params.creator.toHexString());
+  let zoraAddress = event.params.zora.toHexString().toLowerCase();
+  let podsAddress = PODS_ADDRESS.toLowerCase();
+  let origin = zoraAddress == podsAddress ? "pods" : "zora";
+  
+  // Debug logging - you can remove this later
+  log.info("Zora address: {}, Pods address: {}, Origin: {}", [zoraAddress, podsAddress, origin]);
+
+  let creatorUser = User.load(event.params.creator.toHexString().toLowerCase() + origin);
   if (!creatorUser) {
-    creatorUser = createUser(event.params.creator.toHexString(), event.block.timestamp, event.address.toHexString());
+    creatorUser = createUser(event.params.creator.toHexString().toLowerCase(), event.block.timestamp, event.address.toHexString(), origin);
   }
   creatorUser.creatorRewardsTotal = creatorUser.creatorRewardsTotal.plus(event.params.creatorReward);
   creatorUser.rewardsTotal = creatorUser.rewardsTotal.plus(event.params.creatorReward);
   creatorUser.updatedAt = event.block.timestamp;
   creatorUser.save();
 
-  let createReferralUser = User.load(event.params.createReferral.toHexString());
+  const createReferralAddress = event.params.createReferral.toHexString().toLowerCase();
+  let createReferralUser = User.load(createReferralAddress + origin);
   if (!createReferralUser) {
-    createReferralUser = createUser(event.params.createReferral.toHexString(), event.block.timestamp, event.address.toHexString());
+    createReferralUser = createUser(createReferralAddress, event.block.timestamp, event.address.toHexString(), origin);
   }
   createReferralUser.createReferralRewardsTotal = createReferralUser.createReferralRewardsTotal.plus(event.params.createReferralReward);
   createReferralUser.rewardsTotal = createReferralUser.rewardsTotal.plus(event.params.createReferralReward);
   createReferralUser.updatedAt = event.block.timestamp;
   createReferralUser.save();
 
-  let mintReferralUser = User.load(event.params.mintReferral.toHexString());
+  const mintReferralAddress = event.params.mintReferral.toHexString().toLowerCase();
+  let mintReferralUser = User.load(mintReferralAddress + origin);
   if (!mintReferralUser) {
-    mintReferralUser = createUser(event.params.mintReferral.toHexString(), event.block.timestamp, event.address.toHexString());
+    mintReferralUser = createUser(mintReferralAddress, event.block.timestamp, event.address.toHexString(), origin);
   }
   mintReferralUser.mintReferralRewardsTotal = mintReferralUser.mintReferralRewardsTotal.plus(event.params.mintReferralReward);
   mintReferralUser.rewardsTotal = mintReferralUser.rewardsTotal.plus(event.params.mintReferralReward);
   mintReferralUser.updatedAt = event.block.timestamp;
   mintReferralUser.save();
 
-  let firstMinterUser = User.load(event.params.firstMinter.toHexString());
+  const firstMinterAddress = event.params.firstMinter.toHexString().toLowerCase();
+  let firstMinterUser = User.load(firstMinterAddress + origin);
   if (!firstMinterUser) {
-    firstMinterUser = createUser(event.params.firstMinter.toHexString(), event.block.timestamp, event.address.toHexString());
+    firstMinterUser = createUser(firstMinterAddress, event.block.timestamp, event.address.toHexString(), origin);
   }
   firstMinterUser.firstMinterRewardsTotal = firstMinterUser.firstMinterRewardsTotal.plus(event.params.firstMinterReward);
   firstMinterUser.rewardsTotal = firstMinterUser.rewardsTotal.plus(event.params.firstMinterReward);
   firstMinterUser.updatedAt = event.block.timestamp;
   firstMinterUser.save();
 
-  let zoraUser = User.load(event.params.zora.toHexString());
-  if (!zoraUser) {
-    zoraUser = createUser(event.params.zora.toHexString(), event.block.timestamp, event.address.toHexString());
-  }
-  zoraUser.zoraRewardsTotal = zoraUser.zoraRewardsTotal.plus(event.params.zoraReward);
-  zoraUser.rewardsTotal = zoraUser.rewardsTotal.plus(event.params.zoraReward);
-  zoraUser.updatedAt = event.block.timestamp;
-  zoraUser.save();
-
-  let fromUser = User.load(event.params.from.toHexString());
+  const fromAddress = event.params.from.toHexString().toLowerCase();
+  let fromUser = User.load(fromAddress + origin);
   if (!fromUser) {
-    fromUser = createUser(event.params.from.toHexString(), event.block.timestamp, event.address.toHexString());
+    fromUser = createUser(fromAddress, event.block.timestamp, event.address.toHexString(), origin);
   }
 }
 
-export function handleWithdraw(event: WithdrawEvent): void {
-  let fromUser = User.load(event.params.from.toHexString());
-  if (!fromUser) {
-    fromUser = createUser(event.params.from.toHexString(), event.block.timestamp, event.address.toHexString());
-  }
-  let toUser = User.load(event.params.to.toHexString());
-  if (!toUser) {
-    toUser = createUser(event.params.to.toHexString(), event.block.timestamp, event.address.toHexString());
-  }
-
-  toUser.rewardsClaimed = toUser.rewardsClaimed.plus(event.params.amount);
-  toUser.updatedAt = event.block.timestamp;
-  toUser.save();
-}
-
-function createUser(address: string, createdAt: BigInt, source: string): User {
-  let user = new User(address);
+function createUser(address: string, createdAt: BigInt, source: string, origin: string): User {
+  let user = new User(address + origin);
+  user.address = address;
   user.createdAt = createdAt;
   user.updatedAt = createdAt;
   user.rewardsTotal = BigInt.fromI32(0);
-  user.rewardsClaimed = BigInt.fromI32(0);
   user.creatorRewardsTotal = BigInt.fromI32(0);
   user.createReferralRewardsTotal = BigInt.fromI32(0);
   user.mintReferralRewardsTotal = BigInt.fromI32(0);
   user.firstMinterRewardsTotal = BigInt.fromI32(0);
   user.zoraRewardsTotal = BigInt.fromI32(0);
   user.source = source;
+  user.origin = origin;
   user.save();
   return user;
 }
